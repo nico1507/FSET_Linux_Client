774fcce9358578ac53ffa1c0d9278c1ee9e71b46
ruth <ruth@fset-dis.stuve.uni-ulm.de>
2022-01-26 15:02:55 +0100

    Exclude local files from profile sync.

--- ressources/config_files/profile_sync.sh
+++ ressources/config_files/profile_sync.sh
@@ -3,9 +3,9 @@ if [ -d /tmp/$USER/profile/ ]
     then
     if [ -d /tmp/$USER/profile/$USER.linux ]
     then
-        rsync --delete -xurl /tmp/$USER/profile/$USER.linux/ ~/
+        rsync --exclude '.local' --exclude '.cache' --delete -xurl /tmp/$USER/profile/$USER.linux/ ~/
     else
         mkdir -p /tmp/$USER/profile/$USER.linux
     fi
-    (while :; do rsync ~/ --delete -xurl /tmp/$USER/profile/$USER.linux/; sleep 5m; done) & disown
+    (while :; do rsync ~/ --exclude '.local' --exclude '.cache' --delete -xurl /tmp/$USER/profile/$USER.linux/; sleep 5m; done) & disown
 fi
